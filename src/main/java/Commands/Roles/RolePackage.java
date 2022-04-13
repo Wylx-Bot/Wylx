91248f190eb708ee11ab9fc053e543b862e959e3
Avery Black <avery.c.black@gmail.com>
2022-04-13 14:36:24 -0700

    Role Module (#56)
    * addroles
    
    * Role command works
    
    * Add permission check to add/remove role
    
    * Usages
    
    * Fix usage prefix
    
    * List roles which are removed or added
    
    * Move RoleUtil to Core.Util

---
+++ src/main/java/Commands/Roles/RolePackage.java
@@ -0,0 +1,23 @@
+package Commands.Roles;
+
+import Core.Events.Commands.ServerCommand;
+import Core.Events.EventPackage;
+import Core.Events.SilentEvents.SilentEvent;
+
+public class RolePackage extends EventPackage {
+
+    public RolePackage() {
+        super(
+                new ServerCommand[]{
+                        new RoleCommand(),
+                        new AddRoleCommand(),
+                        new RemoveRoleCommand(),
+                }, new SilentEvent[]{}
+        );
+    }
+
+    @Override
+    public String getHeader() {
+        return "Have users self-assign roles";
+    }
+}
---
+++ src/main/java/Core/Util/RoleUtil.java
@@ -0,0 +1,55 @@
+package Core.Util;
+
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Role;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class RoleUtil {
+    public static List<String> commaArrayStripKeyword(String msg, String keyword) {
+        String strippedStr = msg.substring(keyword.length());
+        return Arrays.stream(strippedStr.split(",")).map(String::trim).toList();
+    }
+
+    public static List<Role> getRolesFromStrings(List<String> roles, Guild guild, List<String> invalid) {
+        List<Role> list = new ArrayList<>();
+        for (String roleStr : roles) {
+            try {
+                long id = Long.parseLong(roleStr);
+                Role roleObj = guild.getRoleById(id);
+                if (roleObj != null) {
+                    list.add(roleObj);
+                    continue;
+                }
+            } catch (NumberFormatException e) {
+                // May be
+            }
+
+            // Try search for the role by name
+            List<Role> roleObjs = guild.getRolesByName(roleStr, true);
+            if (roleObjs.size() != 0) {
+                list.addAll(roleObjs);
+            } else if (invalid != null) {
+                invalid.add(roleStr);
+            }
+        }
+
+        return list;
+    }
+
+    public static List<Role> getRolesFromIds(List<Long> roles, Guild guild) {
+        List<Role> list = new ArrayList<>();
+        for (long id : roles) {
+            Role newRole = guild.getRoleById(id);
+            if (newRole != null) {
+                list.add(newRole);
+            } else {
+                // TODO: Non existant role??
+            }
+        }
+
+        return list;
+    }
+}
