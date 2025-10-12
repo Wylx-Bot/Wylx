c5d3e78fdb1b431e15b0c1695234550dd2efcf93
Avery Black <avery.c.black@gmail.com>
2025-10-12 16:06:02 -0700

    Add youtube cipher server support

--- src/main/java/com/wylxbot/wylx/Core/WylxEnvConfig.java
+++ src/main/java/com/wylxbot/wylx/Core/WylxEnvConfig.java
@@ -8,7 +8,9 @@ public class WylxEnvConfig {
     public final String releaseDiscordToken;
     public final String betaDiscordToken;
     public final String betaPrefix;
-    public final String oauthRefreshToken;
+    public final String ytOauthRefreshToken;
+    public final String ytCipherServerUrl;
+    public final String ytCipherServerPassword;
 
     public WylxEnvConfig(Dotenv env) {
         this.release = Boolean.parseBoolean(env.get("RELEASE", "false"));
@@ -16,6 +18,8 @@ public class WylxEnvConfig {
         this.releaseDiscordToken = env.get("DISCORD_TOKEN");
         this.betaDiscordToken = env.get("BETA_DISCORD_TOKEN");
         this.betaPrefix = env.get("BETA_PREFIX");
-        this.oauthRefreshToken = env.get("YT_SRC_OAUTH_REFRESH_TOKEN");
+        this.ytOauthRefreshToken = env.get("YT_SRC_OAUTH_REFRESH_TOKEN");
+        this.ytCipherServerUrl = env.get("YT_SRC_CIPHER_URL");
+        this.ytCipherServerPassword = env.get("YT_SRC_CIPHER_PASSWORD");
     }
 }
