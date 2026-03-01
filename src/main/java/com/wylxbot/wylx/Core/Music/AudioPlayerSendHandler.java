bd51f17c084cb7e31e57d8a5552f18041f9c8861
Avery Black <avery.c.black@gmail.com>
2026-03-01 12:55:38 -0800

    Updated AudioPlayerSendHandler with new lavaplayer example
    Old implementation was broken with the introduction of JDAVE

--- src/main/java/com/wylxbot/wylx/Core/Music/AudioPlayerSendHandler.java
+++ src/main/java/com/wylxbot/wylx/Core/Music/AudioPlayerSendHandler.java
@@ -1,10 +1,9 @@
 package com.wylxbot.wylx.Core.Music;
 
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
-import com.sedmelluq.discord.lavaplayer.track.playback.MutableAudioFrame;
+import com.sedmelluq.discord.lavaplayer.track.playback.AudioFrame;
 import net.dv8tion.jda.api.audio.AudioSendHandler;
 
-import java.nio.Buffer;
 import java.nio.ByteBuffer;
 
 /**
@@ -12,34 +11,25 @@ import java.nio.ByteBuffer;
  * before every call to provide20MsAudio(), we pull the frame in canProvide() and use the frame we already pulled in
  * provide20MsAudio().
  *
- * This is from https://github.com/sedmelluq/lavaplayer/
+ * This is from https://github.com/lavalink-devs/lavaplayer?tab=readme-ov-file#jda-integration
  */
 public class AudioPlayerSendHandler implements AudioSendHandler {
     private final AudioPlayer audioPlayer;
-    private final ByteBuffer buffer;
-    private final MutableAudioFrame frame;
+    private AudioFrame lastFrame;
 
-    /**
-     * @param audioPlayer Audio player to wrap.
-     */
     public AudioPlayerSendHandler(AudioPlayer audioPlayer) {
         this.audioPlayer = audioPlayer;
-        this.buffer = ByteBuffer.allocate(1024);
-        this.frame = new MutableAudioFrame();
-        this.frame.setBuffer(buffer);
     }
 
     @Override
     public boolean canProvide() {
-        // returns true if audio was provided
-        return audioPlayer.provide(frame);
+        lastFrame = audioPlayer.provide();
+        return lastFrame != null;
     }
 
     @Override
     public ByteBuffer provide20MsAudio() {
-        // flip to make it a read buffer
-        ((Buffer) buffer).flip();
-        return buffer;
+        return ByteBuffer.wrap(lastFrame.getData());
     }
 
     @Override
